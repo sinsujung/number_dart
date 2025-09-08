20529d135edec05922b4275966045fa5fbda698f
신 수정 <jhw02045@naver.com>
2025-09-08 15:55:17 +0900

    update random-num-page [2025.09.08]

--- numberDart.js
+++ numberDart.js
@@ -5,6 +5,8 @@
  */
 let pickNum;
 let repeat = true;
+var input = document.getElementById('random-num');
+
 const RandomNum = (min, max) => {
     pickNum = Math.floor(Math.random()* (max - min + 1) + min);
     console.log(pickNum);
@@ -14,35 +16,44 @@ const RandomNum = (min, max) => {
 const handlebreak = () => {
     repeat = false;
     pickNum = null;
+    input.value = null;
     alert("숫자 맞추기를 끝냅니다.");
 };
 
 const handleStart = () => {
     repeat = true;
+    input.value = null;
     pickNum = RandomNum(1, 10);
 };
+
 const handleInputNum = (inputValue) => {
     const numberValue = Number(inputValue)
-
-    while(repeat) {
-        if(numberValue == pickNum) {
-            alert('정답! 숫자를 섞습니다.');
-            RandomNum(1, 10);
-        }
-        else if(numberValue != pickNum) {
-            alert('땡! 다른 숫자를 입력해주세요.');
-        }
-        break;
-    }
-
-    if(pickNum == null || pickNum == undefined) {
+    input.value = null;
+    if(repeat != true) {
         comparison();
+    }
+    else {
+        while(repeat) {
+            if(numberValue == pickNum) {
+                alert('정답! 숫자를 섞습니다.');
+                input.value = null;
+                RandomNum(1, 10);
+            }
+            else if((pickNum != null || pickNum != undefined) &&numberValue != pickNum) {
+                alert('땡! 다른 숫자를 입력해주세요.');
+            }
+            break;
+        }
+
+        if(pickNum == null || pickNum == undefined) {
+            comparison();
+        }
     }
 };
 
 
 const comparison = () => {
-        alert("click -> start!!");
+    alert("click -> start!!");
 };
 
-comparison();
+// comparison();
